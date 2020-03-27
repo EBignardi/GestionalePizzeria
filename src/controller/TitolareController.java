0d1e8596b39795c2be197612a88bc20092236bc3
EBignardi <eros.bignardi@gmail.com>
2020-03-27 10:52:10 +0100

    version 27.03.00 aggiunti vari grafici e "pseudo-codice"

--- src/controller/TitolareController.java
+++ src/controller/TitolareController.java
@@ -3,7 +3,6 @@ package controller;
 import java.net.URL;
 import java.sql.SQLException;
 import java.text.DateFormatSymbols;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -18,6 +17,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
@@ -28,16 +28,24 @@ public class TitolareController implements Initializable {
 	private Label userlbl;
 	
     @FXML // fx:id="chartVendite"
+    private LineChart<String, Number> chartOrdini; // Value injected by FXMLLoader
+    
+    @FXML // fx:id="chartVendite"
     private LineChart<String, Number> chartVendite; // Value injected by FXMLLoader
+    
+
+    @FXML // fx:id="top3clienti"
+    private PieChart top3clienti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="top3clienti"
+    private PieChart top3pizze; // Value injected by FXMLLoader
     
     @FXML
     private CategoryAxis xAxis;
 
     private ObservableList<String> nomeMesi = FXCollections.observableArrayList();
     
-    private ObservableList<Integer> ordiniMese = FXCollections.observableArrayList();
-    
-	public Scene start() throws Exception {		
+    public Scene start() throws Exception {		
 		Parent par = FXMLLoader.load(getClass().getResource("/view/Titolare.fxml"));
 		Scene homeScene = new Scene(par);
 		return homeScene;
@@ -49,8 +57,9 @@ public class TitolareController implements Initializable {
 		userlbl.setText("Welcome Ciani");
 		
 		setGraficoOrdini();
-		
-		
+		setGraficoVendite();
+		setTop3Clienti();
+		setTop3Pizze();
 	}
 
 	public void setGraficoOrdini() {
@@ -61,6 +70,8 @@ public class TitolareController implements Initializable {
 
 		//array ordinato che mi dice il numero di ordini per mese		
 		int[] ordiniMese = new int[12];
+		
+		//estrazione dei dati dal DataBase
 		try {
 			ordiniMese = ClienteDAO.ordiniPerMese();
 		} catch (ClassNotFoundException | SQLException e) {
@@ -77,33 +88,62 @@ public class TitolareController implements Initializable {
 		}
 
 		series.setName("Numero ordini mensili");
-		chartVendite.getData().add(series);
+		chartOrdini.getData().add(series);
 	}
 	
-	public void setGrafico() {
+	public void setGraficoVendite() {
 		// Get an array with the Italian month names
 		String[] mesi = DateFormatSymbols.getInstance(Locale.ITALIAN).getMonths();
 		// Convert it to a list and add it to our ObservableList of months.
 		nomeMesi.addAll(Arrays.asList(mesi));
 
 		//array ordinato che mi dice il numero di ordini per mese		
-		int[] ordiniMese = new int[12];
-		try {
-			ordiniMese = ClienteDAO.ordiniPerMese();
+		int[] fatturatoMese = {100, 50, 200, 300, 150, 250, 400, 650, 200, 300, 150, 250};
+		
+		//estrazione dei dati dal DataBase
+		/*try {
+			fatturatoMese = ;
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Errore nella copiatura array ordiniMese");
 			e.printStackTrace();
-		}
+		}*/
 
 		//caricamento dati nel grafico relativo agli Ordini
 		Series<String, Number> series = new XYChart.Series<String, Number>();
 
 		for (int i = 0; i < 12; i++) {
-			series.getData().add(new XYChart.Data<String, Number>(nomeMesi.get(i), ordiniMese[i]));
+			series.getData().add(new XYChart.Data<String, Number>(nomeMesi.get(i), fatturatoMese[i]));
 		}
 
-		series.setName("Numero ordini mensili");
+		series.setName("Fatturato mensile");
 		chartVendite.getData().add(series);
 	}
+	
+	public void setTop3Clienti() {
+		ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Neri", 50),
+                new PieChart.Data("Grenzi", 30),
+                new PieChart.Data("Panarotto", 20)
+                );
+       
+		top3clienti.setTitle("Top 3 Clienti");
+
+		top3clienti.getData().addAll(pieChartData);
+	}
+
+	public void setTop3Pizze() {
+		ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Salsiccia", 40),
+                new PieChart.Data("Diavola", 37),
+                new PieChart.Data("4 Stagioni", 23)
+                );
+       
+		top3pizze.setTitle("Top 3 Pizze");
+
+		top3pizze.getData().addAll(pieChartData);
+	}
+	
 }
